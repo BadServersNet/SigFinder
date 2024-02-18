8bb0efd44ac7331205ee853d08481cd900565156
BuSheey <6915170+busheezy@users.noreply.github.com>
2024-02-17 19:14:38 -0600

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,64 @@
+import { Worker, isMainThread, parentPort } from 'worker_threads';
 import { getGameData } from './get-game-data';
-import { OS } from './types';
+import { OS, SignatureWithBytes } from './types';
 import {
   getSignaturesWithBytes,
   indexesOf,
   getServerFileAsBytesByOs,
 } from './utils';
 
-async function run() {
-  const gameData = await getGameData();
-  const signatures = getSignaturesWithBytes(gameData);
+const THREADS = 4;
 
-  const linuxServerAsBytes = await getServerFileAsBytesByOs(OS.Linux);
-  const windowsServerAsBytes = await getServerFileAsBytesByOs(OS.Windows);
-
-  signatures.forEach((signature) => {
-    const linuxIndexes = indexesOf(linuxServerAsBytes, signature.linuxBytes);
-    const windowsIndexes = indexesOf(
-      windowsServerAsBytes,
-      signature.windowsBytes,
-    );
-
-    console.log(
-      `[linux]   ${signature.name} found at ${linuxIndexes.length} indexes: ${linuxIndexes}`,
-    );
-    console.log(
-      `[windows] ${signature.name} found at ${windowsIndexes.length} indexes: ${windowsIndexes}`,
-    );
-  });
+interface TaskMessage {
+  task: {
+    signature: SignatureWithBytes;
+    linuxServerAsBytes: number[];
+    windowsServerAsBytes: number[];
+  };
 }
 
-run().catch(console.error);
+async function run() {
+  if (isMainThread) {
+    const gameData = await getGameData();
+
+    const signatures = getSignaturesWithBytes(gameData);
+    const linuxServerAsBytes = await getServerFileAsBytesByOs(OS.Linux);
+    const windowsServerAsBytes = await getServerFileAsBytesByOs(OS.Windows);
+
+    const workerThreads: Worker[] = [];
+
+    for (let i = 0; i < THREADS; i++) {
+      workerThreads.push(new Worker(__filename));
+    }
+
+    signatures.forEach((signature, index) => {
+      const workerIndex = index % THREADS;
+
+      workerThreads[workerIndex].postMessage({
+        task: {
+          signature,
+          linuxServerAsBytes,
+          windowsServerAsBytes,
+          os: index < signatures.length / 2 ? OS.Linux : OS.Windows,
+        },
+      } as TaskMessage);
+    });
+
+    return;
+  }
+
+  if (parentPort == null) {
+    throw new Error('We should have a parent port');
+  }
+
+  parentPort.on('message', (message) => {
+    console.log(`Worker ${process.pid}: Received task`);
+    performTask(message.task);
+  });
+
+  function performTask(task) {
+    console.log('task', task);
+  }
+}
+
+run().catch((err) => console.error(err));
